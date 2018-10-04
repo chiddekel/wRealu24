ca206b02cfeee7878d9594ab932fa0fbb0d6c4be
chiddekel <adam.langowicz@gmail.com>
2018-10-04 21:36:33 +0200

    Udany build

--- android_build_script.sh
+++ android_build_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tns build android --release --compileSdk ~/Android --key-store-path ~/Public/wRealu24.tv.jks --key-store-password Ou3phaiC --key-store-alias wRealu24.tv app --key-store-alias-password Ou3phaiC
+tns build android  --release --key-store-path ~/Public/wRealu24.jks --key-store-password Ou3phaiC --key-store-alias wRealu24 --key-store-alias-password Ou3phaiC
 
